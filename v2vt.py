2495b56deb4ec771101a44601f352cb298f0ce76
huangjackson <wboh010@gmail.com>
2024-04-06 23:06:39 -0700

    Remove termcolor

--- v2vt.py
+++ v2vt.py
@@ -1,7 +1,5 @@
 import argparse
 
-from termcolor import cprint
-
 from tools.utils import check_dependencies, check_models_and_download
 from tools.ffmpeg import extract_audio
 
@@ -32,31 +30,29 @@ if __name__ == '__main__':
     check_dependencies()
     check_models_and_download(ask=True)
 
-    cprint('[Step 1]: Extracting audio from video...',
-           'black', 'on_cyan', attrs=['bold'])
+    print('[Step 1]: Extracting audio from video...')
     extract_audio(video_file_path=args.input,
                   output_audio_file_path='logs/audio.wav')
 
-    cprint('[Step 2]: Removing vocals...', 'black', 'on_cyan', attrs=['bold'])
+    print('[Step 2]: Removing vocals...')
     uvr = UltimateVocalRemover(input_file='logs/audio.wav',
                                output_folder='logs').run()
 
-    cprint('[Step 3]: Slicing audio...', 'black', 'on_cyan', attrs=['bold'])
+    print('[Step 3]: Slicing audio...')
     slicer = Slicer(input_file='logs/audio_Vocals.wav',
                     output_folder='logs/sliced_audio').run()
 
-    cprint('[Step 4]: Transcribing audio...',
-           'black', 'on_cyan', attrs=['bold'])
+    print('[Step 4]: Transcribing audio...')
     transcriber = Transcriber(input_folder='logs/sliced_audio',
                               output_folder='logs').run()
 
-    cprint('[Step 5]: Translating..', 'black', 'on_cyan', attrs=['bold'])
+    print('[Step 5]: Translating..')
     # TODO: Temporarily hard-coded for zh-en translation
     translator = Translator(input_file='logs/transcript.txt',
                             output_file='logs/translation.txt',
                             model_name='zh-en').run()
 
-    cprint('[Step 6]: Executing TTS...', 'black', 'on_cyan', attrs=['bold'])
+    print('[Step 6]: Executing TTS...')
     tts_1a = GetText().execute()
     tts_1b = GetHubertWav32k().execute()
     tts_1c = GetSemantic().execute()
@@ -73,11 +69,9 @@ if __name__ == '__main__':
                          text_language='en',
                          ref_free=True).run()
 
-    cprint('[Step 7]: Executing lip sync...',
-           'black', 'on_cyan', attrs=['bold'])
+    print('[Step 7]: Executing lip sync...')
     lipsync = LipSyncInference(input_video=args.input,
                                input_audio='logs/output.wav',
                                output_path=args.output).run()
 
-    cprint(
-        f'[Final] Done. File written to {args.output}', 'black', 'on_green', attrs=['bold'])
+    print(f'[Final] Done. File written to {args.output}')
